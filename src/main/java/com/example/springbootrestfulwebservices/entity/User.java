0a9b22e5cd97413e6ce2ba776f4bb6689c8ceba2
Alvaro <jlinaresmedalla@gmail.com>
2023-07-25 00:10:36 -0500

    Advance 1

---
+++ src/main/java/com/example/springbootrestfulwebservices/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.springbootrestfulwebservices.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+}
